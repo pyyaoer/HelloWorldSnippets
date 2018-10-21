487be571eb21c2039ffe1a0bafa4bcfba18cfd50
pyyaoer <pyyaoer@gmail.com>
2018-10-22 03:03:10 +0800

    Update table periodically with data from backend

--- Django/Django+Ajax/mysite/myapp/views.py
+++ Django/Django+Ajax/mysite/myapp/views.py
@@ -4,6 +4,18 @@ import json
 
 def ajax_test(request):
     if request.method == "POST":
-        cmd = request.POST.get('cmd')
-        return HttpResponse(json.dumps({"msg": "haha"}))
-    return render(request, "index.html", {"msg": "Hello, World"})
+        req = json.loads(request.body)
+        cmd = req['cmd']
+        if cmd == "1":
+            return HttpResponse(json.dumps({"msg": "haha"}))
+        elif cmd == "2":
+            from random import random
+            data_list = {}
+            for x in req["index"]:
+                data_list[x]= random()
+            return HttpResponse(json.dumps(data_list))
+    name_list = ["alice", "bob", "oscar"]
+    name_json = []
+    for idx, name in enumerate(name_list):
+        name_json.append({"id": idx+1, "name": name})
+    return render(request, "index.html", {"msg": "Hello, World", "names": name_json})
