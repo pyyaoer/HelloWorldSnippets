9f9799b60e02ad230ae2d7ec21b4d1b81e736154
pyyaoer <pyyaoer@gmail.com>
2018-10-21 22:57:08 +0800

    Ajax request and response

--- Django/Django+Ajax/mysite/myapp/urls.py
+++ Django/Django+Ajax/mysite/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ajax_test, name='ajax_test'),
+    path('ajax_test', views.ajax_test, name='ajax_test'),
 ]
--- Django/Django+Ajax/mysite/myapp/views.py
+++ Django/Django+Ajax/mysite/myapp/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import json
+
 
 def ajax_test(request):
+    if request.method == "POST":
+        cmd = request.POST.get('cmd')
+        return HttpResponse(json.dumps({"msg": "haha"}))
     return render(request, "index.html", {"msg": "Hello, World"})
