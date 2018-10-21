90d21edb15ad39176c59d88f8cfc6d56e1ffc346
pyyaoer <pyyaoer@gmail.com>
2018-10-21 17:34:05 +0800

    Basic template in Django with ajax

--- Django/Django+Ajax/mysite/myapp/views.py
+++ Django/Django+Ajax/mysite/myapp/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse("Hello, world.")
+def ajax_test(request):
+    return render(request, "index.html", {"msg": "Hello, World"})
